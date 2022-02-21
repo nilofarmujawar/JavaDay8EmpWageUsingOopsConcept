4d2718e8b4cbd3b5d016af44d598bcff854b616a
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-21 21:38:59 +0530

    Added : Program for Calculate Wage For Month.

--- src/com/bridgelab/EmpWageBuilder.java
+++ src/com/bridgelab/EmpWageBuilder.java
@@ -1,47 +1,66 @@
 package com.bridgelab;
 
-// uc4
+// uc5
 
 /*
-     adding employee salary using switch case statement
+     calculating wages for month
  */
-
 
 public class EmpWageBuilder {
 
     public static final int IS_FULL_TIME = 1;
     public static final int SALARY_PER_HR = 20;
     public static final int IS_PART_TIME = 2;
+    public static final int NO_OF_DAYS = 20;
 
     public static void employeeAttendace() {
 
-        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
-
         int Working_Hr = 0;
+        int empwage = 0;
+        int Totalempwage = 0;
 
-        switch (empCheck) {
+        /*
+        using for loop
+         */
 
-            case IS_FULL_TIME:
-                Working_Hr = 8;   //condition 1
-                break;
+        for (int day = 0; day < NO_OF_DAYS; day++) {
 
-            case IS_PART_TIME:
-                Working_Hr = 4;   //condition 2
-                break;
+            int empCheck = (int) Math.floor((Math.random() * 10) % 3);
 
-            default:
-                Working_Hr = 0;  //default condition print if case1 and 2 fail.
+            /*
+            adding employee salary using switch case statement
+
+             */
+
+            switch (empCheck) {
+
+                case IS_FULL_TIME:      //condtion 1
+                    Working_Hr = 8;
+                    break;
+
+                case IS_PART_TIME:       //condition 2
+                    Working_Hr = 4;
+                    break;
+
+                default:                     // if case1 and case2 is fail then default condition is print
+                    Working_Hr = 0;
+
+            }
+
+            empwage = Working_Hr * SALARY_PER_HR;
+
+            Totalempwage += empwage;
+
+            System.out.println("Emp Wage :  " + empwage);
         }
 
-        double Salary = Working_Hr * SALARY_PER_HR;
-
-        System.out.println("Emplyee Salary is = " + Salary + "  Doller");
+        System.out.println("Total Emp Wage : " + Totalempwage);
     }
-
 
     public static void main(String[] args) {
 
         EmpWageBuilder.employeeAttendace();    //main method
+
     }
 
 }
