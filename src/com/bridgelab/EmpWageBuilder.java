d623aa3c126327f2d4b09a2a6f76800961e896f4
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-21 21:57:53 +0530

    Added : Program for Refractor the Program add the Method.

--- src/com/bridgelab/EmpWageBuilder.java
+++ src/com/bridgelab/EmpWageBuilder.java
@@ -1,71 +1,69 @@
 package com.bridgelab;
 
-// uc6
+// uc7
 
 /*
-    Calculate Wages till a condition of total working hours or
-     days is reached for a month
+    Refactor the Code to write a Class Method to Compute Employee Wage
  */
 
 public class EmpWageBuilder {
 
-    public static final int IS_FULL_TIME = 1;
-    public static final int SALARY_PER_HR = 20;
-    public static final int IS_PART_TIME = 2;
-    public static final int NO_OF_DAYS = 20;
-    public static final int MAX_HR_IN_MONTH = 100;
 
-    public static void employeeAttendace() {
+    static final int IS_PRESENT = 1;
+    static final int IS_PART_TIME = 2;
 
-        int Working_Hr = 0;
+    public static void employeeAttendace(int SALARY_PER_HOUR, int TOTAL_WORK_DAY, int MAX_WORKING_HOUR,
+                                         int FULL_DAY_WORKING_HOUR) {
 
-        int TotalEmpHrs = 0;
-
-        int TotalEmpWorkingDay = 0;
+        int empCheck;
+        int empHr = 0;
+        int totalSalary = 0;
+        int day = 0;
 
         /*
-        using while loop
-         */
+        while loop used
+          */
 
-        while (TotalEmpHrs <= MAX_HR_IN_MONTH && TotalEmpWorkingDay < NO_OF_DAYS) {
+        while (day < TOTAL_WORK_DAY && (empHr + FULL_DAY_WORKING_HOUR) <= MAX_WORKING_HOUR) {
 
-            TotalEmpWorkingDay++;
+            day++;
 
-            int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+            empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
             /*
-             adding employee salary using switch case
+            adding employee salary using switch case
              */
 
             switch (empCheck) {
 
-                case IS_FULL_TIME:
-                    Working_Hr = 8;    //condition 1
+                case IS_PRESENT:                            //condition 1
+                    empHr += FULL_DAY_WORKING_HOUR;
                     break;
 
-                case IS_PART_TIME:
-                    Working_Hr = 4;      //condition 2
+                case IS_PART_TIME:                          //condition 2
+                    empHr += (FULL_DAY_WORKING_HOUR / 2);
                     break;
 
-                default:                //if case 1 and case2 is failed then default condition is print
-                    Working_Hr = 0;
-
+                default:
+                    empHr += 0;             // if case1 and case 2 is failed then default condition print
+                    break;
             }
 
-            TotalEmpHrs += Working_Hr;
-
-            System.out.println("Day :  " + TotalEmpWorkingDay + "   Emp Hr : " + Working_Hr);
+            System.out.println("Working day : " + day + " (Present : " + empCheck + ")");
 
         }
 
-        int totalEmpWage = TotalEmpHrs * SALARY_PER_HR;
+        totalSalary = empHr * SALARY_PER_HOUR;
 
-        System.out.println("Total Emp Wage : " + totalEmpWage);
+        System.out.println("Monthly Emp Wage : " + totalSalary + "\nTotal working hour : " + empHr);
+
     }
 
+    //main method
+    
     public static void main(String[] args) {
 
-        EmpWageBuilder.employeeAttendace();     //main method
+        EmpWageBuilder.employeeAttendace(20, 20, 100, 8);
     }
 
 }
